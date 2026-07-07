b2917025f3233c0593e85d80e54fa171459d8d45
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2026-07-07 14:44:24 +0200

    feat: add skeleton removal logic for Image component

--- src/components/image/Image.vue
+++ src/components/image/Image.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+    import { onMounted, ref } from 'vue'
+
     interface Props {
         as?: string
     }
@@ -6,10 +8,24 @@
     withDefaults(defineProps<Props>(), {
         as: 'div'
     })
+
+    const image = ref<HTMLElement | null>(null)
+
+    onMounted(() => {
+        const element = image.value?.querySelector('img, video, iframe')
+
+        if (!element) return
+
+        const removeSkeleton = () => image.value?.classList.remove('before:skeleton')
+
+        if ('complete' in element && element.complete) removeSkeleton()
+        else if (element instanceof HTMLVideoElement) element.oncanplay = removeSkeleton
+        else element.addEventListener('load', removeSkeleton, { once: true })
+    })
 </script>
 
 <template>
-    <component class="x-image" :is="as">
+    <component class="x-image" :is="as" ref="image">
         <slot></slot>
     </component>
 </template>
