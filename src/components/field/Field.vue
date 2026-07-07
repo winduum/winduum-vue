ad208fa1b707f024dbaffa20bafa10ca3dd3ffd4
Lubomír Blažek <lubomir.blazek@newlogic.cz>
2026-07-07 14:12:12 +0200

    feat: add Form component with validation support and readme

--- src/components/field/Field.vue
+++ src/components/field/Field.vue
@@ -1,15 +1,23 @@
 <script setup lang="ts">
+    import type { ValidateFieldOptions } from 'winduum/src/components/form'
+    import { validateField } from 'winduum/src/components/form'
+
     interface Props {
         as?: string
+        validateOptions?: ValidateFieldOptions
     }
 
-    withDefaults(defineProps<Props>(), {
+    const props = withDefaults(defineProps<Props>(), {
         as: 'div'
     })
+
+    const validate = ({ currentTarget }: Event) => {
+        validateField(currentTarget as HTMLElement, props.validateOptions)
+    }
 </script>
 
 <template>
-    <component class="x-field" :is="as">
+    <component class="x-field" :is="as" @change="validate">
         <slot></slot>
     </component>
 </template>
---
+++ src/components/form/Form.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+    import type { ValidateFormOptions } from 'winduum/src/components/form'
+    import { validateForm } from 'winduum/src/components/form'
+
+    interface Props {
+        validateOptions?: ValidateFormOptions
+    }
+
+    const props = defineProps<Props>()
+
+    const validate = (event: SubmitEvent) => {
+        validateForm(event, props.validateOptions)
+    }
+</script>
+
+<template>
+    <form class="x-form" novalidate @submit="validate">
+        <slot></slot>
+    </form>
+</template>
